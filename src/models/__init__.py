23bccd040a1e7bb5c143e5c2a64adea77fc50fe2
blckgyana <al.saclakow@yandex.ru>
2025-05-29 07:43:30 +0300

    tests

---
+++ src/models/__init__.py
@@ -0,0 +1,5 @@
+from src.models.bookings import BookingsORM
+from src.models.facilities import FacilitiesORM, RoomsFacilitiesORM
+from src.models.hotels import HotelsORM
+from src.models.rooms import RoomsORM
+from src.models.users import UsersORM
